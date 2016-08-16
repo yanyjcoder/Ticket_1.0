b5c83d215075748f765d3a0ea47ad25d67621ac0
vanycc <279931366@qq.com>
2016-08-16 11:44:57 +0800

    strust2框架

---
+++ src/com/ticket/yanyj/action/TestAction.java
@@ -0,0 +1,17 @@
+package com.ticket.yanyj.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3620686942578689657L;
+
+	@Override
+	public String execute() throws Exception {
+
+		return "sucess";
+	}
+}
--- src/com/ticket/yanyj/test/JdbcUtilTest.java
+++ src/com/ticket/yanyj/test/JdbcUtilTest.java
@@ -7,13 +7,11 @@ import org.junit.Test;
 import com.ticket.yanyj.constant.TicketConstant;
 import com.ticket.yanyj.util.JdbcUtil;
 
-@SuppressWarnings("unused")
 public class JdbcUtilTest {
 
 	@Test
 	public void testCreateConnetion() {
-		//Connection con = JdbcUtil.createConnetion(TicketConstant.STR_JDRIVER, TicketConstant.DB_STR, TicketConstant.DB_USER, TicketConstant.DB_PASSWORD);
-		Connection con = null;
+		Connection con = JdbcUtil.createConnetion(TicketConstant.STR_JDRIVER, TicketConstant.DB_STR, TicketConstant.DB_USER, TicketConstant.DB_PASSWORD);
 		JdbcUtil.closeConnetion(con);
 	}
 }
