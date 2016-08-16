355fded97c1cd13c789c1e88ba064ea0c6149c96
vanycc <279931366@qq.com>
2016-08-16 17:30:20 +0800

    1

---
+++ src/com/ticket/yanyj/action/BaseAction.java
@@ -0,0 +1,39 @@
+package com.ticket.yanyj.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport{
+ private static final Logger logger = Logger.getLogger("");
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String render(String text, String contentType)
+	  {
+	    try
+	    {
+	      HttpServletResponse response = ServletActionContext.getResponse();
+	      response.setContentType(contentType);
+	      response.getWriter().write(text);
+	    }
+	    catch (IOException e)
+	    {
+	      logger.error(e.getMessage(), e);
+	    }
+	    return null;
+	  }
+	  
+	  public String renderText(String text)
+	  {
+	    return render(text, "text/plain;charset=UTF-8");
+	  }
+
+}
